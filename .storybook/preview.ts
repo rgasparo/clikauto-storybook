cad5cff236fadc0c4476ad215926bfa6abe0c3b2
Ricardo Ochoa <ricardo.gaspar@clikauto.com>
2023-09-14 23:36:32 -0600

    wip

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,41 @@
-import type { Preview } from '@storybook/react'
+import { withMuiTheme } from './with-mui-theme.decorator';
 
-const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
+// Load Roboto fonts
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    expanded: true,
+    hideNoControlsWarning: true,
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
   },
 }
 
-export default preview
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    title: 'Theme',
+    description: 'Theme for your components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'paintbrush',
+      dynamicTitle: true,
+      items: [
+        { value: 'light', left: '☀️', title: 'Light mode' },
+        { value: 'dark', left: '🌙', title: 'Dark mode' },
+      ],
+    },
+  },
+};
+
+export const decorators = [
+  withMuiTheme,
+];
--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react'
-
 import { Button } from './Button'
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
@@ -8,8 +7,15 @@ const meta: Meta<typeof Button> = {
   component: Button,
   tags: ['autodocs'],
   argTypes: {
-    backgroundColor: {
-      control: 'color',
+    label: {
+      label: '¡Call to action!',
+      description: 'Overwritten description',
+    },
+    color: {
+      color: 'primary',
+    },
+    variant: {
+      control: 'contained',
     },
   },
 }
@@ -20,27 +26,40 @@ type Story = StoryObj<typeof Button>
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
+    label: '¡Call to action!',
+    color:'primary',
+    variant:'contained',
   },
 }
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
+    label: '¡Call to action!',
+    color:'primary',
+    variant:'contained',
+  },
+}
+export const Text: Story = {
+  args: {
+    label: '¡Call to action!',
+    color:'primary',
+    variant:'text',
   },
 }
 
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-}
 
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-}
+// export const Large: Story = {
+//   args: {
+//     label: '¡Call to action!',
+//     color:'primary',
+//     variant:'contained',
+//   },
+// }
+
+// export const Small: Story = {
+//   args: {
+//     label: '¡Call to action!',
+//     color:'primary',
+//     variant:'contained',
+//   },
+// }
